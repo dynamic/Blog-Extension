96fb63e3052e449863674fbf4edabb1ef60fae6a
Nic Horstmeier <nhorstmeier@Nics-MacBook-Pro.local>
2014-01-31 13:53:52 -0600

    Update getHeroImage to return $this->owner->Image() rather than $this->Image()

--- code/BlogEntryExtension.php
+++ code/BlogEntryExtension.php
@@ -1,5 +1,5 @@
 <?php
- 
+
 class BlogEntryExtension extends DataExtension {
 
 	static $db = array(
@@ -10,57 +10,57 @@ class BlogEntryExtension extends DataExtension {
 		'Image' => 'Image',
 		'Thumbnail' => 'Image'
 	);
-	
+
 	public function updateCMSFields(FieldList $fields) {
-		
+
 		// Main Image
 		$ImageField = new UploadField('Image', 'Main Image');
 		$ImageField->getValidator()->allowedExtensions = array('jpg', 'jpeg', 'gif', 'png');
 		$ImageField->setConfig('allowedMaxFileNumber', 1);
 		$ImageField->setFolderName('Uploads/BlogEntries');
 		$fields->addFieldToTab('Root.Images', $ImageField);
-		
+
 		// Thumbnail Image
 		$ImageField = new UploadField('Thumbnail', 'Thumbnail Preview (square)');
 		$ImageField->getValidator()->allowedExtensions = array('jpg', 'jpeg', 'gif', 'png');
 		$ImageField->setConfig('allowedMaxFileNumber', 1);
 		$ImageField->setFolderName('Uploads/BlogEntryThumbs');
 		//$fields->addFieldToTab('Root.Images', $ImageField);
-		
+
 		// featured
 		$fields->addFieldToTab('Root.Main', new CheckboxField('Featured', 'Featured Post (display link in Featured box in sidebar)'), 'Content');
 
 	}
-	
+
 	public function getArticle() {
-		
+
 		if ($this->owner->ThumbnailID) {
 			$extra = '<img src="' . $this->owner->Thumbnail()->setWidth(200)->URL . '" class="left">';
 			$content = $this->owner->Content;
-			
+
 			$pos = strpos($content, '</p>') + 4;
 			$content = substr($content, 0, $pos) . $extra . substr($content, $pos);
 		} else {
 			$content = $this->owner->Content;
 		}
-		
+
 		return $content;
 	}
-	
+
 	// News Archive Grouping
 	public function getMonthCreated() {
         return date('F Y', strtotime($this->owner->Date));
     }
-    
+
     // resize Main image on detail
     public function getHeroImage() {
 	    if ($this->owner->Image()) {
 		    if ($this->owner->Image()->getWidth() > 450 || $this->owner->Image()->getHeight() > 300) {
 			    return $this->owner->Image()->SetRatioSize(450,300);
 		    }
-		    return $this->Image();
+		    return $this->owner->Image();
 	    }
 	    return false;
     }
- 
+
 }
